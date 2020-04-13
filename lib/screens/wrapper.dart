867c490018df550d18a7329e98dae42ec5e7b1f2
Victor Higaki <higvic@gmail.com>
2020-04-13 01:02:28 -0300

    feat: structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'authenticate/authenticate.dart';
+import 'home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    // return either Home or Authenticate widget
+    return Home();
+  }
+}
