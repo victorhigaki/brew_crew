f6e85574c6c64a178b114180d57b4b343c8be319
Victor Higaki <higvic@gmail.com>
2020-04-13 01:39:01 -0300

    feat: add signIn

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -5,8 +5,7 @@ import 'home/home.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // return either Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
