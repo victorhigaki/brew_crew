99c352efa7447f86a532c49a7119e89bd8595995
Victor Higaki <higvic@gmail.com>
2020-04-21 01:11:42 -0300

    feat: bottom sheet

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -12,6 +12,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text('bottom sheet'),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -27,7 +38,12 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
-            )
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text('settings'),
+              onPressed: () => _showSettingsPanel(),
+            ),
           ],
         ),
         body: BrewList(),
